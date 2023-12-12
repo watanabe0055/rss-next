2da638aeaa5096aa507dd13bef8fdccbb19d0659
watanabe0055 <mailok1212+github@gmail.com>
2023-12-12 18:19:29 +0900

    feat: typographyコンポーネントを作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Tag from "@/atoms/tag";
+import Typography from "@/atoms/Typography";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <>
-      <Tag>Tag</Tag>
-    </>
-  );
+  return <></>;
 }
